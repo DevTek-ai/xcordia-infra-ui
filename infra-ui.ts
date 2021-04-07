b882cd1f60257f123bfe8b96a69528b53a3ae2c0
Hussain Mansoor <hussain_raj1@yahoo.com>
2021-04-07 19:00:10 +0800

    Handled environment at stack level

--- infra-ui.ts
+++ infra-ui.ts
@@ -5,7 +5,27 @@ import { InfraStack } from './infra-stack';
 const config = require('config');
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-    stackName: `${config.get('PROJECT_NAME')}-Stack`,
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'qa') {
     
-});
+    new InfraStack(app, get_logical_env_name('Stack'), {
+        stackName: get_logical_env_name('Stack'),
+    });
+}
+else {
+
+    new InfraStack(app, 'InfraStack', {
+        stackName: `${config.get('PROJECT_NAME')}-Stack`,
+        
+    });
+}
+
+function get_logical_env_name(resource_type: string): string {
+
+    let val = `${config.get('PROJECT_NAME')}-${config.get('ENVIRONMENT')}`
+    if (resource_type) {
+        val = val + '-' + resource_type;
+    }
+
+    return val;
+}
